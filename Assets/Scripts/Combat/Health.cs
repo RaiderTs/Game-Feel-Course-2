d288e0322fd05783c45832f8fda75760f0be3dd8
Raider_Ts <alex.tsotsko21@gmail.com>
2025-06-24 20:38:33 +0300

    Add splatter effect on enemy death and update package dependencies

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,26 +1,42 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject _splatterPrefab;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
 
-    private void Start() {
+    private void Start()
+    {
         ResetHealth();
     }
 
-    public void ResetHealth() {
+    public void ResetHealth()
+    {
         _currentHealth = _startingHealth;
     }
 
-    public void TakeDamage(int amount) {
+    public void TakeDamage(int amount)
+    {
         _currentHealth -= amount;
 
-        if (_currentHealth <= 0) {
+        if (_currentHealth <= 0)
+        {
+            SpawnDeathSplatterPrefab();
             Destroy(gameObject);
         }
+    }
+
+    private void SpawnDeathSplatterPrefab()
+    {
+        GameObject newSplatterPrefab = Instantiate(_splatterPrefab, transform.position, transform.rotation);
+        SpriteRenderer deathSplatterSpriteRenderer = newSplatterPrefab.GetComponent<SpriteRenderer>();
+        ColorChanger colorChanger = GetComponent<ColorChanger>(); // получаем компонент ColorChanger
+        Color currentColor = colorChanger.DefaultColor; // получаем цвет из компонента ColorChanger
+
+        deathSplatterSpriteRenderer.color = currentColor;
     }
 }
