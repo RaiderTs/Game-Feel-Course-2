c85628b01daf1c244aa07153197db04b5cf4fbbe
Raider_Ts <alex.tsotsko21@gmail.com>
2025-06-25 18:41:05 +0300

    Add cowboy hat tilt effect to PlayerAnimation for enhanced character movement

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -10,6 +10,8 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] private float _tiltAngle = 20f;
     [SerializeField] private float _tiltSpeed = 5f;
     [SerializeField] private Transform _characterSpriteTransform;
+    [SerializeField] private Transform _cowboyHatTransform;
+    [SerializeField] private float _cowboyHatTiltModifier = 2f;
 
     private void Update()
     {
@@ -56,11 +58,20 @@ public class PlayerAnimation : MonoBehaviour
         //     Создаётся новая целевая ориентация (targetCharacterRotation), где угол по оси Z устанавливается в targetAngle, а остальные оси (X и Y) остаются как были.
         //     Применяется этот новый поворот к спрайту игрока, чтобы визуально наклонить его влево, вправо или вернуть в исходное положение в зависимости от направления движения.
 
+        // Player Sprite
         Quaternion currentCharacterRotation = _characterSpriteTransform.rotation;
         Quaternion targetCharacterRotation = Quaternion.Euler(currentCharacterRotation.eulerAngles.x,
             currentCharacterRotation.eulerAngles.y, targetAngle);
 
         _characterSpriteTransform.rotation = Quaternion.Lerp(currentCharacterRotation, targetCharacterRotation,
             _tiltSpeed * Time.deltaTime);
+
+        // Cowboy hat sprite
+        Quaternion currentHatRotation = _cowboyHatTransform.rotation;
+        Quaternion targetHatRotation = Quaternion.Euler(currentHatRotation.eulerAngles.x,
+            currentHatRotation.eulerAngles.y, -targetAngle / _cowboyHatTiltModifier);
+
+        _cowboyHatTransform.rotation = Quaternion.Lerp(currentHatRotation, targetHatRotation,
+            _tiltSpeed * _cowboyHatTiltModifier * Time.deltaTime);
     }
 }
