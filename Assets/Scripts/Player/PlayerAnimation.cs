7d10dd43545c8f886ed7ca0b692fdbbcfe49aeda
Raider_Ts <alex.tsotsko21@gmail.com>
2025-06-25 18:14:52 +0300

    Add tilt effect to player sprite during movement

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -7,10 +7,14 @@ public class PlayerAnimation : MonoBehaviour
 {
     // пишем логику чтобы не было эффекта если игрок подпрыгнул
     [SerializeField] private ParticleSystem _moveDustVFX;
+    [SerializeField] private float _tiltAngle = 20f;
+    [SerializeField] private float _tiltSpeed = 5f;
+    [SerializeField] private Transform _characterSpriteTransform;
 
     private void Update()
     {
         DetectMoveDust();
+        ApplyTilt();
     }
 
     private void DetectMoveDust()
@@ -29,5 +33,33 @@ public class PlayerAnimation : MonoBehaviour
                 _moveDustVFX.Stop();
             }
         }
+    }
+
+    private void ApplyTilt() // применить наклон
+    {
+        float targetAngle;
+
+        if (PlayerController.Instance.MoveInput.x < 0f)
+        {
+            targetAngle = _tiltAngle;
+        }
+        else if (PlayerController.Instance.MoveInput.x > 0f)
+        {
+            targetAngle = -_tiltAngle;
+        }
+        else
+        {
+            targetAngle = 0f;
+        }
+
+        // Получается текущий поворот спрайта игрока (_characterSpriteTransform.rotation).
+        //     Создаётся новая целевая ориентация (targetCharacterRotation), где угол по оси Z устанавливается в targetAngle, а остальные оси (X и Y) остаются как были.
+        //     Применяется этот новый поворот к спрайту игрока, чтобы визуально наклонить его влево, вправо или вернуть в исходное положение в зависимости от направления движения.
+
+        Quaternion currentCharacterRotation = _characterSpriteTransform.rotation;
+        Quaternion targetCharacterRotation = Quaternion.Euler(currentCharacterRotation.eulerAngles.x,
+            currentCharacterRotation.eulerAngles.y, targetAngle);
+
+        _characterSpriteTransform.rotation = targetCharacterRotation;
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector2 MoveInput => _frameInput.Move;
+
     public static Action OnJump; // паттерн обсервер
 
     public static PlayerController Instance;
